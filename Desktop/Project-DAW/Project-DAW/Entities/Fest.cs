57bd6616cb1988e97c953f49c5222ef40f2eb5e7
Elias2510 <eliasmilosi2002@gmail.com>
2024-02-15 19:03:05 +0200

    first

---
+++ Desktop/Project-DAW/Project-DAW/Entities/Fest.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_DAW.Entities
+{
+    public class Fest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Oras { get; set; }
+
+        public ICollection<Vehicul> Vehicule { get; } 
+    }
+}
